2984e44b28df088abef426f993bb2767daae4641
boxtecnich <142419344+boxtecnich@users.noreply.github.com>
2023-08-16 15:58:20 +0200

    initial commit

---
+++ imsearch.php
@@ -0,0 +1,76 @@
+<?php require_once("res/x5engine.php"); ?>
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>Buscar - boxtecnich</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="generator" content="Incomedia WebSite X5 Pro 2022.1.7 - www.websitex5.com" />
+		<meta property="og:locale" content="es" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://localhost/imsearch.php" />
+		<meta property="og:title" content="Buscar" />
+		<meta property="og:site_name" content="boxtecnich" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="stylesheet" href="style/reset.css?2022-1-7-0" media="screen,print" />
+		<link rel="stylesheet" href="style/print.css?2022-1-7-0" media="print" />
+		<link rel="stylesheet" href="style/style.css?2022-1-7-0" media="screen,print" />
+		<link rel="stylesheet" href="style/template.css?2022-1-7-0" media="screen" />
+		<link rel="stylesheet" href="pcss/imsearch.css?2022-1-7-0-638277981684373185" media="screen,print" />
+		<script src="res/jquery.js?2022-1-7-0"></script>
+		<script src="res/x5engine.js?2022-1-7-0" data-files-version="2022-1-7-0"></script>
+		<script>
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+			x5engine.utils.currentPagePath = 'imsearch.php';
+			x5engine.boot.push(function () { x5engine.imPageToTop.initializeButton({}); });
+		</script>
+		
+	</head>
+	<body>
+		<div id="imPageExtContainer">
+			<div id="imPageIntContainer">
+				<div id="imHeaderBg"></div>
+				<div id="imFooterBg"></div>
+				<div id="imPage">
+					<header id="imHeader">
+						<h1 class="imHidden">Buscar - boxtecnich</h1>
+						<div id="imHeaderObjects"></div>
+					</header>
+					<div id="imStickyBarContainer">
+						<div id="imStickyBarGraphics"></div>
+						<div id="imStickyBar">
+							<div id="imStickyBarObjects"></div>
+						</div>
+					</div>
+					<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+					<div id="imSideBar">
+						<div id="imSideBarObjects"></div>
+					</div>
+					<div id="imContentGraphics"></div>
+					<main id="imContent">
+						<a id="imGoToCont"></a>
+						<div id="imSearchPage">
+						<h2 id="imPgTitle">Resultados de búsqueda</h2>
+						<?php
+						$search = new imSearch();
+						$keys = isset($_GET['search']) ? @htmlspecialchars($_GET['search']) : "";
+						$page = isset($_GET['page']) ? @htmlspecialchars($_GET['page']) : 0;
+						$type = isset($_GET['type']) ? @htmlspecialchars($_GET['type']) : "pages"; ?>
+						<div class="searchPageContainer">
+						<?php echo $search->search($keys, $page, $type); ?>
+						</div>
+						</div>
+						
+					</main>
+					<footer id="imFooter">
+						<div id="imFooterObjects"></div>
+					</footer>
+				</div>
+				<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a></span>
+			</div>
+		</div>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html>
